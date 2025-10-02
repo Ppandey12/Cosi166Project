bb29302f605bda51df4bcf7b9c88963ae2ec1c91
Ppandey12 <paraspandey827@gmail.com>
2025-10-02 14:26:14 -0400

    done with general page for notes

--- DemoEditor/demo/src/components/Sidebar.jsx
+++ DemoEditor/demo/src/components/Sidebar.jsx
@@ -5,20 +5,14 @@ const Sidebar = () => {
     <div className="sideBar">
       <div className="sidebar_header">
         <h1>Class Notes</h1>
-        <button>Add Note</button>
       </div>
 
       <div className="notes">
         <div className="note">
           <div className="note_title">
-            <strong>Title</strong>
-            <button>Delete Note</button>
+           <p className="title"> Class 1 </p>
+            <p className="Date">last modofied [date]</p>
           </div>
-
-          <p>Note Preview</p>
-          <small className="note_meta">
-            last modofied [date]
-          </small>
         </div>
       </div>
     </div>
